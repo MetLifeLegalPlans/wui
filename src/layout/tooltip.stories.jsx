3c4f3fd7a78e2130e8af23a507550b8f0b8613c6
Mark Rawls <markrawls96@gmail.com>
2019-11-20 17:51:36 -0500

    Add react-styleguidist and remove stories

--- src/layout/tooltip.stories.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Tooltip from './tooltip';
-
-storiesOf('Layout', module).add('Tooltip', () => <Tooltip title="I am a tooltip!" />);
