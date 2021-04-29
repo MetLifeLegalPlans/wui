6c7184f22293601258d3ca870ced234fd28df050
Mark Rawls <markrawls96@gmail.com>
2021-04-29 18:31:10 -0400

    Clean slate

--- src/basics/Countdown.md
+++
@@ -1,5 +0,0 @@
-A 10 second countdown clock:
-
-```jsx
-<Countdown end={Date.now() + 10000} />
-```
--- src/utils.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line import/prefer-default-export
-export const capitalize = string => string.charAt(0).toUpperCase() + string.slice(1);
