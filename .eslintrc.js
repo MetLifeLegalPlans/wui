28a4b2e9f542712539c77f3959eada46a4346792
Mark Rawls <markrawls96@gmail.com>
2021-04-29 18:31:33 -0400

    Only export a minimal MUI theme object

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = {
+  parser: '@babel/eslint-parser',
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    // for loops should not be restricted
+    'no-restricted-syntax': ['off'],
+    // this is a NodeJS app
+    'no-console': ['off']
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: './src'
+      }
+    }
+  }
+};
