177b4d6b4b7fc96306f5bea6d38fa9c346d95cdd
Mark Rawls <markrawls96@gmail.com>
2021-06-07 19:25:59 -0400

    Add spacer component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
-// TODO: Remove when we have more things to export
-// eslint-disable-next-line import/prefer-default-export
 export { default as theme } from './theme';
+export { default as Spacer } from './Spacer';
