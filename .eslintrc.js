20c69bca0b7e97410f2bbc20c9a325863cc96e44
Mark Rawls <markrawls96@gmail.com>
2021-06-07 19:32:34 -0400

    Add eslint react configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
-"use strict";
+'use strict';
 
 module.exports = {
   parser: '@babel/eslint-parser',
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'prettier', 'plugin:react/recommended'],
   plugins: ['prettier'],
   rules: {
     // for loops should not be restricted
     'no-restricted-syntax': ['off'],
     // this is a NodeJS app
-    'no-console': ['off']
+    'no-console': ['off'],
   },
   settings: {
     'import/resolver': {
       node: {
-        paths: './src'
-      }
-    }
-  }
+        paths: './src',
+      },
+    },
+  },
 };
--- src/Spacer.js
+++ src/Spacer.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import Hidden from '@material-ui/core/Hidden';
 
