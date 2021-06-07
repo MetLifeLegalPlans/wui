3c23f126ab48df55e48b781686da6d8853020706
Mark Rawls <markrawls96@gmail.com>
2021-06-07 19:25:59 -0400

    Add spacer component

---
+++ src/Spacer.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import Hidden from '@material-ui/core/Hidden';
+
+const Spacer = ({ h: horizontal, v: vertical, inline, children, ...props }) => (
+  <Hidden {...props}>
+    <span
+      style={{
+        display: inline ? 'inline-flex' : 'flex',
+        height: vertical,
+        width: horizontal,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      {children}
+    </span>
+  </Hidden>
+);
+
+Spacer.propTypes = {
+  h: PropTypes.number,
+  v: PropTypes.number,
+  inline: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+Spacer.defaultProps = {
+  h: 0,
+  v: 0,
+  children: null,
+  inline: false,
+};
+
+export default Spacer;
