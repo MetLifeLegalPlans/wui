9dfd7627b68b444046d4b8ec69815a426c3fb644
Mark Rawls <markrawls96@gmail.com>
2021-06-16 19:09:16 -0400

    Put imports in alphabetical order

--- src/Modal.js
+++ src/Modal.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
