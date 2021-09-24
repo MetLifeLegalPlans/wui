ec52e5e5f7231356e57fdbdb86e004ba10af23e6
epicoding95 <kelcrisp007@icloud.com>
2021-09-23 21:46:00 -0700

    fix the index js file to export the theme object

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export { makeStyles, useStyles } from './theme';
+export { makeStyles, useStyles, theme } from './theme';
 export { default as Spacer } from './Spacer';
 export { default as Modal } from './Modal';
