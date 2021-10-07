1d09020b250f1ad442c158b828abfd066b95295b
epicoding95 <kelcrisp007@icloud.com>
2021-10-07 06:03:45 -0700

    FRONTEND: fix the spacer component to no longer use the deprecated Hidden component

--- src/Spacer.js
+++ src/Spacer.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Hidden from '@material-ui/core/Hidden';
 
-const Spacer = ({ h: horizontal, v: vertical, inline, children, ...props }) => (
-  <Hidden {...props}>
-    <span
-      style={{
-        display: inline ? 'inline-flex' : 'flex',
-        height: vertical,
-        width: horizontal,
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      {children}
-    </span>
-  </Hidden>
+const Spacer = ({ h: horizontal, v: vertical, inline }) => (
+  <span
+    style={{
+      display: inline ? 'inline-flex' : 'flex',
+      height: vertical,
+      width: horizontal,
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  />
 );
 
 Spacer.propTypes = {
   h: PropTypes.number,
   v: PropTypes.number,
   inline: PropTypes.bool,
-  children: PropTypes.node,
 };
 
 Spacer.defaultProps = {
   h: 0,
   v: 0,
-  children: null,
   inline: false,
 };
 
