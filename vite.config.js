688a7894c959f5c6e66eb44a31b1b2542de31062
kelar <kelcrisp007@icloud.com>
2023-01-25 07:25:19 -0800

    update publish script

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
-import { resolve } from 'node:path'
+/* eslint-disable import/no-extraneous-dependencies */
+import { resolve } from 'node:path';
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import * as packageJson from './package.json'
-
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import * as packageJson from './package.json';
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'wui',
-      fileName: (format) => `wui.${format}.js`,
+      fileName: format => `wui.${format}.js`,
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@emotion/react': 'ReactEmotion',
+          '@emotion/styled': 'emStyled',
+        },
+      },
     },
   },
-})
+});
